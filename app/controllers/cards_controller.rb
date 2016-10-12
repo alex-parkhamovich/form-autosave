e1dd4e562695156e3d3b19f7b7aebd9ad86c9047
Alexander <lml.alexander.lml@gmail.com>
2016-10-12 16:53:21 +0300

    bug fix

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,43 +2,24 @@ class CardsController < ApplicationController
 
   def new
     @card = Card.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
   end
 
   def show
     @card = Card.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
   end
 
   def create
     @card = Card.new(card_params)
 
-    respond_to do |format|
-      if @card.save
-        format.html { redirect_to @user, notice: 'Card was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @card.save
+      redirect_to @card
+    else 
+      render 'new'
     end
   end 
 
   def index
       @cards = Card.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
   end
 
   private
